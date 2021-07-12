23651e90fb101bc740863113321ab2f894077acd
ErgoAsh <wiedzmin1371@gmail.com>
2021-07-12 23:03:04 +0200

    Add more control over Bluetooth data transfer

--- RoboPrinter.Server/src/main.cpp
+++ RoboPrinter.Server/src/main.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
@@ -11,9 +12,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 uint8_t servonum = 0;
 
+uint8_t character_counter = 0;
+String bluetoothResponse = "";
+
 void setup()
 {
   Serial.begin(9600);
+  Serial1.begin(9600);
 
   pwm.begin();
   /*
@@ -22,12 +27,12 @@ void setup()
    * you get the PWM update frequency you're expecting!
    * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
    * is used for calculating things like writeMicroseconds()
-   * Analog servos run at ~50 Hz updates, It is importaint to use an
+   * Analog servos run at ~50 Hz updates, It is important to use an
    * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
    * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
    *    the I2C PCA9685 chip you are setting the value for.
    * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-   *    expected value (50Hz for most ESCs)
+   *    expected value (50Hz for most ESC-s)
    * Setting the value here is specific to each individual I2C PCA9685 chip and
    * affects the calculations for the PWM update frequency. 
    * Failure to correctly set the int.osc value will cause unexpected PWM results
@@ -40,12 +45,24 @@ void setup()
 
 void loop()
 {
-  int sensorValue = analogRead(A0);
-  //Serial.println(sensorValue);
+  if (Serial1.available() > 0)
+  {
+    char character = Serial1.read();
 
-  int sensorValue2 = analogRead(A1);
-  Serial.println(sensorValue2);
+    if ((character == '\n' || character == '\r') && bluetoothResponse != "")
+    {
+      char output[32];
+      sprintf(output, "OK: %s", bluetoothResponse);
+      Serial.println(output);
 
-  pwm.setPWM(servonum, 0, map(sensorValue, 0, 1023, SERVOMIN, SERVOMAX));
-  delay(10);
+      //pwm.setPWM(servonum, 0, map(sensorValue, 0, 1023, SERVOMIN, SERVOMAX));
+
+      character_counter = 0;
+      bluetoothResponse = "";
+    }
+    else
+    {
+      bluetoothResponse += character;
+    }
+  }
 }
